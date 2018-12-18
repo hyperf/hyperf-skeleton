bf09b767c364537b9fbf27ee33958d2c79b3c9f1
李铭昕 <715557344@qq.com>
2018-12-18 18:28:43 +0800

    Modify test route

--- config/routes.php
+++ config/routes.php
@@ -9,10 +9,7 @@ use Hyperflex\HttpServer\Router\Router;
 
 $router->addRoute(['GET', 'POST', 'HEAD'], '/index', [\App\Controllers\IndexController::class, 'index']);
 $router->addRoute(['GET', 'POST', 'HEAD'], '/user/{id:\d+}', [\App\Controllers\IndexController::class, 'user']);
-$router->addRoute(['GET', 'POST', 'HEAD'], '/int', [\App\Controllers\IndexController::class, 'int']);
+$router->addRoute(['GET', 'POST', 'HEAD'], '/int', 'App\Controllers\IndexController@int');
 
 Router::get('/', [\App\Controllers\IndexController::class, 'index']);
-Router::get('/call', function () {
-    return 'Hi, Hyperflex.';
-});
-// $router->addRoute(['GET', 'POST', 'HEAD'], '/int', [\App\Controllers\IndexController::class, 'int']);
+Router::get('/index/index', 'App\Controllers\IndexController@index');
