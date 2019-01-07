038454257518abe5cc4481035341103f7b6568ec
huangzhhui <huangzhwork@gmail.com>
2019-01-07 23:12:33 +0800

    Added env

--- config/databases.php
+++ config/databases.php
@@ -3,10 +3,10 @@
 return [
     'default' => [
         'driver' => 'mysql',
-        'host' => 'localhost',
-        'database' => 'hyperf',
-        'username' => 'root',
-        'password' => '910123',
+        'host' => env('DB_HOST', 'localhost'),
+        'database' => env('DB_DATABASE', 'hyperf'),
+        'username' => env('DB_USERNAME', 'root'),
+        'password' => env('DB_PASSWORD', ''),
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix' => '',
