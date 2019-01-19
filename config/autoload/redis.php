8ef879b46f1857942dd6ec6be51e918fbe118c47
李铭昕 <715557344@qq.com>
2019-01-19 14:39:53 +0800

    Support redis pool option user defined.

--- config/autoload/redis.php
+++ config/autoload/redis.php
@@ -5,6 +5,13 @@ return [
         'host' => env('REDIS_HOST', 'localhost'),
         'auth' => env('REDIS_AUTH', ''),
         'port' => (int)env('REDIS_PORT', 6379),
-        'db' => (int)env('REDIS_DB', 0)
+        'db' => (int)env('REDIS_DB', 0),
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 10,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+        ],
     ],
 ];
