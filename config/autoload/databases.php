36e2ab13e8aa601703370c3b5ca16ff07fbfab10
李铭昕 <715557344@qq.com>
2019-01-19 14:36:37 +0800

    Support database pool option user defined.

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -11,5 +11,12 @@ return [
         'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
         'max_idle_time' => (float)env('DB_MAX_IDLE_TIME', 60),
+        'pool' => [
+            'min_connections' => 1,
+            'max_connections' => 10,
+            'connect_timeout' => 10.0,
+            'wait_timeout' => 3.0,
+            'heartbeat' => -1,
+        ],
     ],
 ];
