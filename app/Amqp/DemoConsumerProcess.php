8d525308a843d13346cf7d1d2575a8dbb1cbedea
huangzhhui <huangzhwork@gmail.com>
2019-01-27 01:04:17 +0800

    If connect to AMQP Server failed greater than 5 times, then sleep the process

--- app/Amqp/DemoConsumerProcess.php
+++ app/Amqp/DemoConsumerProcess.php
@@ -14,12 +14,28 @@ namespace App\Amqp;
 
 use Hyperf\Amqp\Consumer;
 use Hyperf\Process\Process;
+use Hyperf\Utils\Str;
 
 class DemoConsumerProcess extends Process
 {
-    public function handle()
+    public function handle(): void
     {
-        $consumer = $this->container->get(Consumer::class);
-        $consumer->consume(new DemoConsumer());
+        $try = 5;
+        for ($k = 0; $k < $try; $k++) {
+            try {
+                $consumer = $this->container->get(Consumer::class);
+                $consumer->consume(new DemoConsumer());
+            } catch (\Throwable $e) {
+                if (Str::contains($e->getMessage(), [
+                    'stream_socket_client(): unable to connect to',
+                ])) {
+                    printf('Cannot connect to AMQP Server due to [%s].' . PHP_EOL, $e->getMessage());
+                    if ($k >= 4) {
+                        printf('Process[%s] sleeped.' . PHP_EOL, static::class);
+                        sleep(3650 * 24 * 3600);
+                    }
+                }
+            }
+        }
     }
 }
