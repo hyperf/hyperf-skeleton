434fa0a61bc0bda2bbaac6479154a9bfa4d1320b
huangzhhui <huangzhwork@gmail.com>
2019-02-27 13:38:15 +0800

    Added config-center config gile

---
+++ config/autoload/config-center.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'adapter' => 'apollo',
+    'apollo' => [
+        'server' => 'http://127.0.0.1:8080',
+        'appid' => 'test',
+        'cluster' => 'default',
+        'namespaces' => [
+            'application',
+        ],
+    ],
+];
