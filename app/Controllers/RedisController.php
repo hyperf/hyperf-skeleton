4e7975c7992db93393844c1b3ca3477ed83d830f
huangzhhui <huangzhwork@gmail.com>
2019-03-12 18:18:36 +0800

    Added redis multi test case

--- app/Controllers/RedisController.php
+++ app/Controllers/RedisController.php
@@ -1,20 +1,28 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.org
+ * @document https://wiki.hyperf.org
+ * @contact  group@hyperf.org
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
 namespace App\Controllers;
 
-
 use Hyperf\Di\Annotation\Inject;
-use Hyperf\Redis\Redis;
+use Hyperf\Utils\Context;
 
 /**
- * @\Hyperf\HttpServer\Annotation\AutoController()
+ * @\Hyperf\HttpServer\Annotation\AutoController
  */
 class RedisController
 {
-
     /**
-     * @Inject()
-     * @var Redis
+     * @Inject
+     * @var \Redis
      */
     private $redis;
 
@@ -24,4 +32,15 @@ class RedisController
         return $this->redis->get('test');
     }
 
+    public function multi()
+    {
+        $this->redis->multi();
+        for ($n = 5; --$n;) {
+            $this->redis->set('test', time());
+            $this->redis->get('test');
+        }
+        $result = $this->redis->exec();
+        var_dump(spl_object_id(Context::get('redis.connection')));
+        return $result;
+    }
 }
