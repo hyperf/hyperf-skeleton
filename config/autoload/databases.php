c871621b6ea3950ee35b8d8f47f5560688279b96
李铭昕 <715557344@qq.com>
2019-03-17 17:54:11 +0800

    Move max_idle_time.

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -10,13 +10,13 @@ return [
         'charset' => env('DB_CHARSET', 'utf8'),
         'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
         'prefix' => env('DB_PREFIX', ''),
-        'max_idle_time' => (float)env('DB_MAX_IDLE_TIME', 60),
         'pool' => [
             'min_connections' => 1,
             'max_connections' => 10,
             'connect_timeout' => 10.0,
             'wait_timeout' => 3.0,
             'heartbeat' => -1,
+            'max_idle_time' => (float)env('DB_MAX_IDLE_TIME', 60),
         ],
         'cache' => [
             'handler' => \Hyperf\DbConnection\Cache\Handler\RedisHandler::class,
--- config/autoload/redis.php
+++ config/autoload/redis.php
@@ -6,13 +6,13 @@ return [
         'auth' => env('REDIS_AUTH', ''),
         'port' => (int)env('REDIS_PORT', 6379),
         'db' => (int)env('REDIS_DB', 0),
-        'max_idle_time' => (float)env('REDIS_MAX_IDLE_TIME', 60),
         'pool' => [
             'min_connections' => 1,
             'max_connections' => 10,
             'connect_timeout' => 10.0,
             'wait_timeout' => 3.0,
             'heartbeat' => -1,
+            'max_idle_time' => (float)env('REDIS_MAX_IDLE_TIME', 60),
         ],
     ],
     'config2' => [
