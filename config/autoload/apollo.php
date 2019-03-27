a74e988cc9435f89d799a94befd683de22159381
huangzhhui <huangzhwork@gmail.com>
2019-03-27 15:02:16 +0800

    Renamed config-center.php to apollo.php

---
+++ config/autoload/apollo.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'enable' => false,
+    'server' => 'http://127.0.0.1:8080',
+    'appid' => 'test',
+    'cluster' => 'default',
+    'namespaces' => [
+        'application',
+    ],
+    'interval' => 5,
+];
