f6084b0fcd911606143c096e0534756a5d8e41b2
李铭昕 <715557344@qq.com>
2019-04-11 14:50:23 +0800

    Update config.

--- config/autoload/processes.php
+++ config/autoload/processes.php
@@ -1,6 +1,7 @@
 <?php
+
 return [
-    // \Hyperf\Queue\Process\ConsumerProcess::class,
+    Hyperf\AsyncQueue\Process\ConsumerProcess::class,
     // \App\Processes\DemoProcess::class,
     // \App\Amqp\DemoConsumerProcess::class,
     // \Hyperf\ConfigApollo\Process\ConfigFetcherProcess::class,
