4cfcfce1ea7958a8ddec18b9a5159026762cecc9
李铭昕 <715557344@qq.com>
2019-04-16 12:33:30 +0800

    Update `router.php`

--- config/routes.php
+++ config/routes.php
@@ -4,20 +4,20 @@ use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/index', [\App\Controller\IndexController::class, 'index']);
 Router::addRoute(['GET', 'POST', 'HEAD'], '/user/{id:\d+}', [\App\Controller\IndexController::class, 'user']);
-Router::addRoute(['GET', 'POST', 'HEAD'], '/int', 'App\Controllers\IndexController@int');
+Router::addRoute(['GET', 'POST', 'HEAD'], '/int', 'App\Controller\IndexController@int');
 
 Router::get('/', [\App\Controller\IndexController::class, 'index']);
-Router::get('/index/static', 'App\Controllers\IndexController@staticIndex');
-Router::get('/index/index', 'App\Controllers\IndexController@index');
-Router::get('/index/sleep', 'App\Controllers\IndexController@sleep');
-Router::get('/index/database', 'App\Controllers\IndexController@database');
-Router::get('/index/model', 'App\Controllers\IndexController@model');
-Router::get('/index/redis', 'App\Controllers\IndexController@redis');
-Router::get('/index/incr', 'App\Controllers\IndexController@incr');
-Router::get('/index/guzzle_handler', 'App\Controllers\IndexController@guzzleHandler');
-Router::get('/index/job', 'App\Controllers\IndexController@job');
-Router::get('/index/amqp', 'App\Controllers\IndexController@amqp');
-Router::get('/index/cache', 'App\Controllers\IndexController@cache');
+Router::get('/index/static', 'App\Controller\IndexController@staticIndex');
+Router::get('/index/index', 'App\Controller\IndexController@index');
+Router::get('/index/sleep', 'App\Controller\IndexController@sleep');
+Router::get('/index/database', 'App\Controller\IndexController@database');
+Router::get('/index/model', 'App\Controller\IndexController@model');
+Router::get('/index/redis', 'App\Controller\IndexController@redis');
+Router::get('/index/incr', 'App\Controller\IndexController@incr');
+Router::get('/index/guzzle_handler', 'App\Controller\IndexController@guzzleHandler');
+Router::get('/index/job', 'App\Controller\IndexController@job');
+Router::get('/index/amqp', 'App\Controller\IndexController@amqp');
+Router::get('/index/cache', 'App\Controller\IndexController@cache');
 
 Router::addGroup(
     '/v2', function () {
@@ -27,11 +27,11 @@ Router::addGroup(
 
 Router::addServer('grpc', function () {
     Router::addGroup('/grpc.hi', function () {
-        Router::post('/sayHello', 'App\Controllers\HiController@sayHello');
+        Router::post('/sayHello', 'App\Controller\HiController@sayHello');
     });
 });
 
 Router::addServer('innerHttp', function () {
-    Router::get('/', 'App\Controllers\IndexController@index');
+    Router::get('/', 'App\Controller\IndexController@index');
 });
 
