3b67043348f462456b46b9a2f790f46c01bb2dd1
daydaygo <daydaygo@vip.qq.com>
2019-05-17 16:46:21 +0800

    add pdo options

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -25,5 +25,13 @@ return [
             'ttl' => 3600 * 24,
             'load_script' => true,
         ],
+        'options' => [
+            // default
+            PDO::ATTR_CASE => PDO::CASE_NATURAL,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+            PDO::ATTR_STRINGIFY_FETCHES => false,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ],
     ],
 ];
