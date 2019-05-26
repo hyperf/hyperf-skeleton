ff16fec55393339616f79434ac3b784cf706f18f
李铭昕 <715557344@qq.com>
2019-05-26 15:54:17 +0800

    Added command.

--- app/Command/DemoCommand.php
+++ app/Command/DemoCommand.php
@@ -4,35 +4,42 @@ declare(strict_types=1);
 /**
  * This file is part of Hyperf.
  *
- * @link     https://hyperf.org
- * @document https://wiki.hyperf.org
- * @contact  group@hyperf.org
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
  * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
 
 namespace App\Command;
 
-use App\Model\User;
-use Hyperf\Amqp\Pool\PoolFactory;
-use Hyperf\Elasticsearch\ClientBuilder;
-use Hyperf\Guzzle\ClientFactory;
-use PhpAmqpLib\Connection\AbstractConnection;
+use Hyperf\Command\AbstractCommand;
+use Hyperf\Framework\Annotation\Command;
 use Psr\Container\ContainerInterface;
-use Swoole\Coroutine;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
-class DemoCommand extends Command
+/**
+ * @Command
+ */
+class DemoCommand extends AbstractCommand
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
     public function __construct(ContainerInterface $container)
     {
-        parent::__construct('demo:test');
         $this->container = $container;
+
+        parent::__construct('demo:command');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    public function configure()
     {
-        $output->writeln('You can do something...');
+        $this->setDescription('Hyperf Demo Command');
+    }
+
+    public function handle()
+    {
+        $this->output->writeln('You can do something...');
     }
 }
