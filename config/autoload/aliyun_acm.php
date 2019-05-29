35949aeb1c5a47a8db97c53f7ba0857be90f75b5
huangzhhui <huangzhwork@gmail.com>
2019-05-30 02:28:28 +0800

    Added aliyun acm config file

---
+++ config/autoload/aliyun_acm.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'enable' => env('ALIYUN_ACM_ENABLE', false),
+    'namespace' => env('ALIYUN_ACM_NAMESPACE'),
+    'data_id' => env('ALIYUN_ACM_DATA_ID'),
+    'access_key' => env('ALIYUN_ACM_ACCESS_KEY'),
+    'secret_key' => env('ALIYUN_ACM_SECRET_KET'),
+];
