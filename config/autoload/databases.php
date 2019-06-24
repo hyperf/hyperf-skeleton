7a1d66c70557aff6a24e2324428455d4bbcaf6d2
xuchunlong <574331590@qq.com>
2019-06-24 17:00:32 +0800

    no message

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -15,6 +15,7 @@ return [
         'driver' => env('DB_DRIVER', 'mysql'),
         'host' => env('DB_HOST', 'localhost'),
         'database' => env('DB_DATABASE', 'hyperf'),
+        'port' => env('DB_PORT', 3306),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', ''),
         'charset' => env('DB_CHARSET', 'utf8'),
