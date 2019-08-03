96865a1cfd86472f0dd3e584acaa39bdcab53ff5
sleep <sleep@kaituocn.com>
2019-08-03 14:43:43 +0800

    Update bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -21,8 +21,6 @@ date_default_timezone_set('Asia/Shanghai');
 
 !defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 
-Runtime::enableCoroutine(true);
-
 require BASE_PATH.'/vendor/autoload.php';
 
 $container = require BASE_PATH.'/config/container.php';
