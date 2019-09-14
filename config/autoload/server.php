3f44f18961f7ecade3a34147d3009afbddfd3f79
张奇峰 <zhangqf@inesadt.com>
2019-09-15 02:27:41 +0800

    Swoole函数中发生的错误记录在hyperf.log文件中

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -31,6 +31,8 @@ return [
         'enable_coroutine' => true,
         'worker_num' => swoole_cpu_num(),
         'pid_file' => BASE_PATH . '/runtime/hyperf.pid',
+        'log_level' => 0,
+        'log_file' => BASE_PATH . 'runtime/logs/hyperf.log',
         'open_tcp_nodelay' => true,
         'max_coroutine' => 100000,
         'open_http2_protocol' => true,
