67500b2f01e0c57dc52aaa38509378e77d69c689
98P <weizhuang_l@163.com>
2019-11-07 17:51:18 +0800

    Update Zipkin tracer config
    更新zipkin配置文件

--- installer/resources/tracer/opentracing.php
+++ installer/resources/tracer/opentracing.php
@@ -13,24 +13,28 @@ declare(strict_types=1);
 use Zipkin\Samplers\BinarySampler;
 
 return [
+    'default' => 'zikpin',
     'enable' => [
         'guzzle' => false,
         'redis' => false,
         'db' => false,
         'method' => false,
     ],
-    'zipkin' => [
-        'app' => [
-            'name' => env('APP_NAME', 'skeleton'),
-            // Hyperf will detect the system info automatically as the value if ipv4, ipv6, port is null
-            'ipv4' => '127.0.0.1',
-            'ipv6' => null,
-            'port' => 9501,
+    'tracer' => [
+        'zipkin' => [
+            'app' => [
+                'name' => env('APP_NAME', 'skeleton'),
+                // Hyperf will detect the system info automatically as the value if ipv4, ipv6, port is null
+                'ipv4' => '127.0.0.1',
+                'ipv6' => null,
+                'port' => 9501,
+            ],
+            'options' => [
+                'endpoint_url' => env('ZIPKIN_ENDPOINT_URL', 'http://localhost:9411/api/v2/spans'),
+                'timeout' => env('ZIPKIN_TIMEOUT', 1),
+            ],
+            'sampler' => BinarySampler::createAsAlwaysSample(),
+            'driver' => Hyperf\Tracer\Adapter\ZipkinTracerFactory::class,
         ],
-        'options' => [
-            'endpoint_url' => env('ZIPKIN_ENDPOINT_URL', 'http://localhost:9411/api/v2/spans'),
-            'timeout' => env('ZIPKIN_TIMEOUT', 1),
-        ],
-        'sampler' => BinarySampler::createAsAlwaysSample(),
     ],
 ];
