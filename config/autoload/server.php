b1429918152676c88be38dcee8cda2437cfc8ef7
liuruikang <liuruikang@360.cn>
2020-01-06 11:27:10 +0800

    配置发送输出缓存区内存尺寸

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -36,6 +36,7 @@ return [
         'open_http2_protocol' => true,
         'max_request' => 100000,
         'socket_buffer_size' => 2 * 1024 * 1024,
+        'buffer_output_size' => 2 * 1024 * 1024,
     ],
     'callbacks' => [
         SwooleEvent::ON_BEFORE_START => [Hyperf\Framework\Bootstrap\ServerStartCallback::class, 'beforeStart'],
