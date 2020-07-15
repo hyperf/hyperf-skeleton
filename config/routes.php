e9443f185a54d00f64825fe88091026f7ac83841
sy-records <52o@qq52o.cn>
2020-07-15 12:04:24 +0800

    Added default route for favicon.ico

--- config/routes.php
+++ config/routes.php
@@ -12,3 +12,7 @@ declare(strict_types=1);
 use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+
+Router::get('/favicon.ico', function () {
+    return '';
+});
