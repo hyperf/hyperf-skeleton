16d8aec72ac6745851f43102644f5e01b4b2f094
wuweixin <wuweixin@gmail.com>
2021-02-22 19:53:24 +0800

    按照字母顺序调整 use 包的引用
    虽说从功能上没有任何变化，但更符合 IDE 包引入的顺序

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-use Hyperf\Server\Server;
 use Hyperf\Server\Event;
+use Hyperf\Server\Server;
 use Swoole\Constant;
 
 return [
