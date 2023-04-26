bb7902a329f1318d8da716e76f365583a5d6f082
liyunfan2015 <35674916+411906015@users.noreply.github.com>
2023-04-26 20:07:09 +0800

    Fixed the prompt for make() (#86)

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -6,4 +6,7 @@ namespace PHPSTORM_META {
     override(\Hyperf\Context\Context::get(0), map(['' => '@']));
     override(\make(0), map(['' => '@']));
     override(\di(0), map(['' => '@']));
+    override(\Hyperf\Support\make(0), map(['' => '@']));
+    override(\Hyperf\Support\optional(0), type(0));
+    override(\Hyperf\Tappable\tap(0), type(0));
 }
