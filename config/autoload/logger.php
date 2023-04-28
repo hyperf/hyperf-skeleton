5c3b043ff99be22094c0f419b4ff834b5f369781
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-04-28 11:49:45 +0800

    🛠️refactor(logger) change level constant for Monolog logger.

--- config/autoload/logger.php
+++ config/autoload/logger.php
@@ -15,7 +15,7 @@ return [
             'class' => Monolog\Handler\StreamHandler::class,
             'constructor' => [
                 'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
-                'level' => \Monolog\Level::Debug,
+                'level' => Monolog\Logger::DEBUG,
             ],
         ],
         'formatter' => [
